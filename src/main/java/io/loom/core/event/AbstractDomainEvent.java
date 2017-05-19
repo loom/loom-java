efdc1c075a0bbd9bcadb5afa0ee61acced61b40c
mhyeon.lee <mhyeon.lee@navercorp.com>
2017-05-19 12:13:51 +0900

    AbstractDomainEvent 가 LazyInitializedDomainEvent 를 구현합니다.

--- src/main/java/io/loom/core/event/AbstractDomainEvent.java
+++ src/main/java/io/loom/core/event/AbstractDomainEvent.java
@@ -1,27 +1,46 @@
 package io.loom.core.event;
 
+import io.loom.core.aggregate.VersionedAggregate;
+
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
-public abstract class AbstractDomainEvent implements DomainEvent {
-    private final UUID aggregateId;
-    private final long version;
-    private final ZonedDateTime occurrenceTime;
+public abstract class AbstractDomainEvent implements LazyInitializedDomainEvent {
+    private UUID aggregateId;
+    private long version;
+    private ZonedDateTime occurrenceTime;
+
+    protected AbstractDomainEvent() {
+    }
 
     protected AbstractDomainEvent(UUID aggregateId, long version, ZonedDateTime occurrenceTime) {
-        if (aggregateId == null) {
-            throw new IllegalArgumentException("The parameter 'aggregateId' cannot be null.");
+        this.checkHeaderPropertiesSet(aggregateId, version, occurrenceTime);
+        this.aggregateId = aggregateId;
+        this.version = version;
+        this.occurrenceTime = occurrenceTime;
+    }
+
+    @Override
+    public final void afterHeaderPropertiesSets(VersionedAggregate versionedAggregate) {
+        if (this.isInitialized()) {
+            throw new IllegalStateException("DomainEvent is already initialized.");
         }
-        if (version < 1) {
-            throw new IllegalArgumentException("The parameter 'version' must be greater than 0.");
-        }
-        if (occurrenceTime == null) {
-            throw new IllegalArgumentException("The parameter 'occurrenceTime' cannot be null.");
-        }
+
+        UUID aggregateId = versionedAggregate.getId();
+        long version = versionedAggregate.getVersion() + 1;
+        ZonedDateTime occurrenceTime = ZonedDateTime.now();
+        this.checkHeaderPropertiesSet(aggregateId, version, occurrenceTime);
 
         this.aggregateId = aggregateId;
         this.version = version;
         this.occurrenceTime = occurrenceTime;
+    }
+
+    @Override
+    public final boolean isInitialized() {
+        return this.aggregateId != null
+                && this.version > 0
+                && this.occurrenceTime != null;
     }
 
     @Override
@@ -37,5 +56,18 @@ public abstract class AbstractDomainEvent implements DomainEvent {
     @Override
     public final ZonedDateTime getOccurrenceTime() {
         return this.occurrenceTime;
+    }
+
+    private void checkHeaderPropertiesSet(
+            UUID aggregateId, long version, ZonedDateTime occurrenceTime) {
+        if (aggregateId == null) {
+            throw new IllegalArgumentException("The parameter 'aggregateId' cannot be null.");
+        }
+        if (version < 1) {
+            throw new IllegalArgumentException("The parameter 'version' must be greater than 0.");
+        }
+        if (occurrenceTime == null) {
+            throw new IllegalArgumentException("The parameter 'occurrenceTime' cannot be null.");
+        }
     }
 }
