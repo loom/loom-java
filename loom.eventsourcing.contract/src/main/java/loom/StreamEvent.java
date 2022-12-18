2eb5218cef1503a93149cc2a8efc64fc09a81c6c
Gyuwon Yi <gyuwon@live.com>
2022-12-18 22:59:33 +0900

    Define class StreamEvent<T>

---
+++ loom.eventsourcing.contract/src/main/java/loom/StreamEvent.java
@@ -0,0 +1,14 @@
+package loom;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public final class StreamEvent<T> {
+    private final String streamId;
+    private final long version;
+    private final LocalDateTime raisedTimeUtc;
+    private final T payload;
+}
