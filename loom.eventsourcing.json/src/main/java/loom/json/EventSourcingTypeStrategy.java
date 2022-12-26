aaee929ae78f4e0426835a5495ea08bc7d72be59
Gyuwon Yi <gyuwon@live.com>
2022-12-26 23:53:20 +0900

    Add class EventSourcingTypeStrategy
    EventSourcingTypeStrategy class implements TypeStrategy. It formats and
    restores event sourcing message types.

---
+++ loom.eventsourcing.json/src/main/java/loom/json/EventSourcingTypeStrategy.java
@@ -0,0 +1,99 @@
+package loom.json;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Optional;
+import loom.eventsourcing.StreamCommand;
+import loom.eventsourcing.StreamEvent;
+
+public final class EventSourcingTypeStrategy implements TypeStrategy {
+
+    private final TypeStrategy payloadStrategy;
+
+    public EventSourcingTypeStrategy(TypeStrategy payloadStrategy) {
+        this.payloadStrategy = payloadStrategy;
+    }
+
+    @Override
+    public Optional<String> tryFormatType(Object value) {
+        if (value instanceof StreamCommand) {
+            StreamCommand<?> command = (StreamCommand<?>) value;
+            Object payload = command.getPayload();
+            String payloadType = payloadStrategy.formatType(payload);
+            return Optional.of("stream-command:" + payloadType);
+        } else if (value instanceof StreamEvent) {
+            StreamEvent<?> event = (StreamEvent<?>) value;
+            Object payload = event.getPayload();
+            String payloadType = payloadStrategy.formatType(payload);
+            return Optional.of("stream-event:" + payloadType);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Type> tryResolveType(String formattedType) {
+        String[] parts = formattedType.split(":", 2);
+        if (parts.length == 2) {
+            String prefix = parts[0];
+            String formattedPayloadType = parts[1];
+            Optional<Type> payloadType =
+                payloadStrategy.tryResolveType(formattedPayloadType);
+            if (prefix.equals("stream-command")) {
+                return payloadType.map(StreamCommandType::new);
+            } else if (prefix.equals("stream-event")) {
+                return payloadType.map(StreamEventType::new);
+            } else {
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private static final class StreamCommandType implements ParameterizedType {
+        private final Type payloadType;
+
+        public StreamCommandType(Type payloadType) {
+            this.payloadType = payloadType;
+        }
+
+        @Override
+        public Type[] getActualTypeArguments() {
+            return new Type[] { payloadType };
+        }
+
+        @Override
+        public Type getRawType() {
+            return StreamCommand.class;
+        }
+
+        @Override
+        public Type getOwnerType() {
+            return null;
+        }
+    }
+
+    private static final class StreamEventType implements ParameterizedType {
+        private final Type payloadType;
+
+        public StreamEventType(Type payloadType) {
+            this.payloadType = payloadType;
+        }
+
+        @Override
+        public Type[] getActualTypeArguments() {
+            return new Type[] { payloadType };
+        }
+
+        @Override
+        public Type getRawType() {
+            return StreamEvent.class;
+        }
+
+        @Override
+        public Type getOwnerType() {
+            return null;
+        }
+    }
+}
---
+++ loom.unittest/src/test/java/test/loom/json/EventSourcingTypeStrategy_specs.java
@@ -0,0 +1,112 @@
+package test.loom.json;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import autoparams.AutoSource;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Optional;
+import loom.eventsourcing.StreamCommand;
+import loom.eventsourcing.StreamEvent;
+import loom.json.ClassNameTypeStrategy;
+import loom.json.EventSourcingTypeStrategy;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class EventSourcingTypeStrategy_specs {
+
+    @ParameterizedTest
+    @AutoSource
+    void tryFormatType_correctly_formats_type_of_stream_command(
+        ClassNameTypeStrategy payloadStrategy,
+        StreamCommand<CreateUser> command
+    ) {
+        EventSourcingTypeStrategy sut =
+            new EventSourcingTypeStrategy(payloadStrategy);
+
+        Optional<String> actual = sut.tryFormatType(command);
+
+        assertThat(actual).contains("stream-command:test.loom.json.CreateUser");
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void tryFormatType_correctly_formats_type_of_stream_event(
+        ClassNameTypeStrategy payloadStrategy,
+        StreamEvent<UserCreated> event
+    ) {
+        EventSourcingTypeStrategy sut =
+            new EventSourcingTypeStrategy(payloadStrategy);
+
+        Optional<String> actual = sut.tryFormatType(event);
+
+        assertThat(actual).contains("stream-event:test.loom.json.UserCreated");
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void tryResolveType_returns_empty_optional_for_invalid_formatted_type(
+        ClassNameTypeStrategy payloadStrategy,
+        String formattedType
+    ) {
+        EventSourcingTypeStrategy sut =
+            new EventSourcingTypeStrategy(payloadStrategy);
+
+        Optional<Type> actual = sut.tryResolveType(formattedType);
+
+        assertThat(actual).isEmpty();
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void tryResolveType_returns_correct_type_for_formatted_stream_command_type(
+        ClassNameTypeStrategy payloadStrategy
+    ) {
+        // Arrange
+        EventSourcingTypeStrategy sut =
+            new EventSourcingTypeStrategy(payloadStrategy);
+        String formattedType = "stream-command:test.loom.json.CreateUser";
+
+        // Act
+        Optional<Type> actual = sut.tryResolveType(formattedType);
+
+        // Assert
+        assertThat(actual).hasValueSatisfying(value -> {
+            assertThat(value).isInstanceOf(ParameterizedType.class);
+
+            ParameterizedType parameterizedType = (ParameterizedType) value;
+
+            assertThat(parameterizedType.getRawType())
+                    .isEqualTo(StreamCommand.class);
+
+            assertThat(parameterizedType.getActualTypeArguments())
+                    .containsExactly(CreateUser.class);
+        });
+    }
+
+    @ParameterizedTest
+    @AutoSource
+    void tryResolveType_returns_correct_type_for_formatted_stream_event_type(
+        ClassNameTypeStrategy payloadStrategy
+    ) {
+        // Arrange
+        EventSourcingTypeStrategy sut =
+            new EventSourcingTypeStrategy(payloadStrategy);
+        String formattedType = "stream-event:test.loom.json.UserCreated";
+
+        // Act
+        Optional<Type> actual = sut.tryResolveType(formattedType);
+
+        // Assert
+        assertThat(actual).hasValueSatisfying(value -> {
+            assertThat(value).isInstanceOf(ParameterizedType.class);
+
+            ParameterizedType parameterizedType = (ParameterizedType) value;
+
+            assertThat(parameterizedType.getRawType())
+                    .isEqualTo(StreamEvent.class);
+
+            assertThat(parameterizedType.getActualTypeArguments())
+                    .containsExactly(UserCreated.class);
+        });
+    }
+}
