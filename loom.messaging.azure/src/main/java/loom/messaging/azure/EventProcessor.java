45332b7d80fa52c96f6009aa4e89c2a17ec91fb3
Flora Jo <flora.jo@healingpaper.com>
2023-02-07 11:51:36 +0900

    Add EventProcessor class and define process method

---
+++ loom.messaging.azure/src/main/java/loom/messaging/azure/EventProcessor.java
@@ -0,0 +1,10 @@
+package loom.messaging.azure;
+
+import com.azure.messaging.eventhubs.EventData;
+
+public class EventProcessor {
+
+    public EventProcessor() {}
+
+    public void process(EventData eventData) {}
+}
